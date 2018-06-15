e01ddeda0adc54eba74bb107c928f041482f541a
Chase Phillips <cmp@chromium.org>
2018-06-15 14:58:34 -0700

    .eslintrc.js: add prettier config

--- third_party/todomvc/vuejs-idb/.eslintrc.js
+++ third_party/todomvc/vuejs-idb/.eslintrc.js
@@ -12,10 +12,14 @@ module.exports = {
     "google",
     "plugin:vue/recommended",
   ],
+  plugins: [
+    "prettier"
+  ],
   // add your custom rules here
   'rules': {
     'indent': [2, 2],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'prettier/prettier': 'error'
   }
 }

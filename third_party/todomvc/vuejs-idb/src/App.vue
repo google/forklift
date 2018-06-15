7e5dea35efb9315543ad2e35891a8419fd44f0b6
Chase Phillips <cmp@chromium.org>
2018-06-15 13:34:44 -0700

    fix eslint errors

--- third_party/todomvc/vuejs-idb/src/App.vue
+++ third_party/todomvc/vuejs-idb/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <Todos></Todos>
+    <Todos/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
+import Todos from './components/Todos';
 
 export default {
-    name: 'app',
-    components: {
-        Todos
-    }
-}
+  name: 'App',
+  components: {
+    Todos,
+  },
+};
 </script>

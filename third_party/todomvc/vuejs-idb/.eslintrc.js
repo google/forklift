374e122444129df107d9421a57098c21c4d00d31
Chase Phillips <cmp@chromium.org>
2018-06-15 14:25:51 -0700

    .eslintrc.js: set indent to +2

--- third_party/todomvc/vuejs-idb/.eslintrc.js
+++ third_party/todomvc/vuejs-idb/.eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    'indent': [2, 2],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
   }
 }

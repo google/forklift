a0394b8d346f90c9b64909578aa2307cff7c51d6
Victor Costan <costan@gmail.com>
2018-08-20 03:47:06 -0700

    Modernize idb-vanilla's app class.

--- third_party/todomvc/vanilla-idb/js/app.js
+++ third_party/todomvc/vanilla-idb/js/app.js
@@ -22,44 +22,42 @@
  */
 
 /*global app, $on */
-(function () {
-    'use strict';
+(() => {
+  'use strict';
 
-    /**
-     * Sets up a brand new Todo list.
+  class Todo {
+    /** Sets up a brand new Todo list.
      *
      * @param {string} name The name of your new to do list.
      */
-    function Todo(name) {
-        this.storage = new app.Store(name);
-        this.model = new app.Model(this.storage);
-        this.template = new app.Template();
-        this.view = new app.View(this.template);
-        this.controller = new app.Controller(this.model, this.view);
+    constructor(name) {
+      this.storage = new app.Store(name);
+      this.model = new app.Model(this.storage);
+      this.template = new app.Template();
+      this.view = new app.View(this.template);
+      this.controller = new app.Controller(this.model, this.view);
     }
+  }
 
-    var todo = new Todo('vanilla-idb');
+  const todo = new Todo('vanilla-idb');
 
-    // Only auto-open the DB if ?open=0 is not passed in the URL.
-    var openStore = true;
-    const parsedUrl = new URL(window.location.href);
-    if (parsedUrl.searchParams.get("open") === "0") {
-        openStore = false;
-    }
+  // Only auto-open the DB if ?open=0 is not passed in the URL.
+  const parsedUrl = new URL(window.location.href);
+  const openStore = (parsedUrl.searchParams.get("open") !== "0");
 
-    if (openStore) {
-        todo.storage.open({populated: false}, function () {
-            // Force the page to update after the DB is opened.
-            todo.controller._filter(true);
-        });
-    }
+  if (openStore) {
+    todo.storage.open({ populated: false }, () => {
+      // Force the page to update after the DB is opened.
+      todo.controller._filter(true);
+    });
+  }
 
-    function setView() {
-        todo.controller.setView(document.location.hash);
-    }
-    $on(window, 'load', setView);
-    $on(window, 'hashchange', setView);
+  const setView = () => {
+    todo.controller.setView(document.location.hash);
+  };
+  $on(window, 'load', setView);
+  $on(window, 'hashchange', setView);
 
-    // Export todo to the window object.
-    window.todo = todo;
+  // Export todo to the window object.
+  window.todo = todo;
 })();

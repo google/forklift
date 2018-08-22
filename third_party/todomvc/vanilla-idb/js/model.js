54ccd837ee3a03fdeeb8e24ef34d40424e89deb5
Victor Costan <costan@gmail.com>
2018-08-21 18:26:07 -0700

    Modernize vanilla-idb's model. (#8)

--- third_party/todomvc/vanilla-idb/js/model.js
+++ third_party/todomvc/vanilla-idb/js/model.js
@@ -21,125 +21,125 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-(function (window) {
-    'use strict';
-
+((window) => {
+  class Model {
     /**
      * Creates a new Model instance and hooks up the storage.
      *
      * @constructor
      * @param {object} storage A reference to the client side storage class
      */
-    function Model(storage) {
-        this.storage = storage;
+    constructor(storage) {
+      this.storage = storage;
     }
 
     /**
      * Creates a new todo model
      *
      * @param {string} [title] The title of the task
-     * @param {function} [callback] The callback to fire after the model is created
+     * @param {function} [callback] The callback to fire after the model is
+     *                              created
      */
-    Model.prototype.create = async function (title, callback) {
-        title = title || '';
-        callback = callback || function () {};
+    async create(title, callback) {
+      title = title || '';
+      callback = callback || (() => {});
 
-        var newItem = {
-            title: title.trim(),
-            completed: false,
-            createdAt: Date.now(),
-            dueAt: Date.now()
-        };
+      const newItem = {
+        title: title.trim(),
+        completed: false,
+        createdAt: Date.now(),
+        dueAt: Date.now()
+      };
 
-        await this.storage.save(newItem, callback);
-    };
+      await this.storage.save(newItem, callback);
+    }
 
     /**
      * Finds and returns a model in storage. If no query is given it'll simply
-     * return everything. If you pass in a string or number it'll look that up as
-     * the ID of the model to find. Lastly, you can pass it an object to match
-     * against.
+     * return everything. If you pass in a string or number it'll look that up
+     * as the ID of the model to find. Lastly, you can pass it an object to
+     * match against.
      *
      * @param {string|number|object} [query] A query to match models against
-     * @param {function} [callback] The callback to fire after the model is found
+     * @param {function} [callback] The callback to fire after the model is
+     *                              found
      *
      * @example
      * model.read(1, func); // Will find the model with an ID of 1
      * model.read('1'); // Same as above
-     * //Below will find a model with foo equalling bar and hello equalling world.
+     * // (below) Find a model with foo equalling bar and hello equalling world.
      * model.read({ foo: 'bar', hello: 'world' });
      */
-    Model.prototype.read = async function (query, callback) {
-        var queryType = typeof query;
-        callback = callback || function () {};
+    async read(query, callback) {
+      const queryType = typeof query;
+      callback = callback || (() => {});
 
-        if (queryType === 'function') {
-            callback = query;
-            return await this.storage.findAll(callback);
-        } else if (queryType === 'string' || queryType === 'number') {
-            query = parseInt(query, 10);
-            await this.storage.find({ id: query }, callback);
-        } else {
-            await this.storage.find(query, callback);
-        }
-    };
+      if (queryType === 'function') {
+        callback = query;
+        return await this.storage.findAll(callback);
+      } else if (queryType === 'string' || queryType === 'number') {
+        query = parseInt(query, 10);
+        await this.storage.find({ id: query }, callback);
+      } else {
+        await this.storage.find(query, callback);
+      }
+    }
 
     /**
-     * Updates a model by giving it an ID, data to update, and a callback to fire when
-     * the update is complete.
+     * Updates a model by giving it an ID, data to update, and a callback to
+     * fire when the update is complete.
      *
      * @param {number} id The id of the model to update
      * @param {object} data The properties to update and their new value
-     * @param {function} callback The callback to fire when the update is complete.
+     * @param {function} callback The callback to fire when the update is
+     *                            complete
      */
-    Model.prototype.update = async function (id, data, callback) {
-        await this.storage.save(data, callback, id);
-    };
+    async update(id, data, callback) {
+      await this.storage.save(data, callback, id);
+    }
 
-    /**
-     * Removes a model from storage
+    /** Removes a model from storage
      *
      * @param {number} id The ID of the model to remove
-     * @param {function} callback The callback to fire when the removal is complete.
+     * @param {function} callback The callback to fire when the removal is
+     *                            complete
      */
-    Model.prototype.remove = async function (id, callback) {
-        await this.storage.remove(id, callback);
-    };
+    async remove(id, callback) {
+      await this.storage.remove(id, callback);
+    }
 
-    /**
-     * WARNING: Will remove ALL data from storage.
+    /** WARNING: Will remove ALL data from storage.
      *
-     * @param {function} callback The callback to fire when the storage is wiped.
+     * @param {function} callback The callback to fire when the storage is wiped
      */
-    Model.prototype.removeAll = async function (callback) {
-        await this.storage.drop(callback);
-    };
+    async removeAll(callback) {
+      await this.storage.drop(callback);
+    }
 
-    /**
-     * Returns a count of all todos
-     */
-    Model.prototype.getCount = async function (callback) {
-        var todos = {
-            active: 0,
-            completed: 0,
-            total: 0
-        };
+    /** Returns a count of all todos. */
+    async getCount(callback) {
+      const todos = {
+        active: 0,
+        completed: 0,
+        total: 0
+      };
 
-        await this.storage.findAll(async function (data) {
-            data.forEach(function (todo) {
-                if (todo.completed) {
-                    todos.completed++;
-                } else {
-                    todos.active++;
-                }
+      await this.storage.findAll(async (data) => {
+        data.forEach((todo) => {
+          if (todo.completed) {
+            todos.completed++;
+          } else {
+            todos.active++;
+          }
 
-                todos.total++;
-            });
-            callback(todos);
+          todos.total++;
         });
-    };
+        callback(todos);
+      });
+    }
+  }
 
-    // Export to window
-    window.app = window.app || {};
-    window.app.Model = Model;
+  // Export to window
+  window.app = window.app || {};
+  window.app.Model = Model;
 })(window);

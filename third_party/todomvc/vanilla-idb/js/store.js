6663d327cb9c33af681899b5f1d9454172928900
Victor Costan <pwnall@chromium.org>
2018-10-29 09:48:14 -0700

    Use more Promises in the IndexedDB wrapper.

--- third_party/todomvc/vanilla-idb/js/store.js
+++ third_party/todomvc/vanilla-idb/js/store.js
@@ -23,59 +23,164 @@
 
 /*jshint eqeqeq:false */
 ((window) => {
+  class DatabaseUtils {
+    /** Wrap an IndexedDB request into a Promise.
+     *
+     * This should not be used for open().
+     *
+     * @param {IDBRequest} request the request to be wrapped
+     * @returns {Promise<Object>} promise that resolves with the request's
+     *     result, or rejects with an error
+     */
+    static async promiseForRequest(request) {
+      return new Promise((resolve, reject) => {
+        request.onsuccess = event => { resolve(event.target.result); };
+        request.onerror = event => { reject(event.target.error); };
+        request.onblocked = event => {
+          reject(event.target.error ||
+                  (new Error("blocked by other database connections")));
+        };
+        request.onupgradeneeded = event => {
+          reject(event.target.error ||
+                  (new Error("unexpected upgradeneeded event")));
+        };
+      });
+    }
+
+    /** Wrap an IndexedDB database open request into a Promise.
+     *
+     * This is intended to be used by open().
+     *
+     * @param {IDBOpenDBRequest} request the request to be wrapped
+     * @returns {Promise<{database: idbDatabase, transaction: IDBTransaction?}>}
+     *     promise that resolves with an object whose "database" property is the
+     *     newly opened database; if an upgradeneeded event is received, the
+     *     "transaction" property holds the upgrade transaction
+     */
+    static async promiseForOpenRequest(request) {
+      return new Promise((resolve, reject) => {
+        request.onsuccess = event => {
+          resolve({ database: event.target.result, transaction: null });
+        };
+        request.onerror = event => { reject(event.target.error); };
+        request.onblocked = event => {
+          reject(event.target.error ||
+                  (new Error("blocked by other database connections")));
+        };
+        request.onupgradeneeded = event => {
+          resolve({
+            database: event.target.result,
+            transaction: event.target.transaction
+          });
+        };
+      });
+    }
+
+    /** Wrap an IndexedDB transaction into a Promise.
+     *
+     * @param {IDBTransaction} transaction the transaction to be wrapped
+     * @returns {Promise<Object>} promise that resolves with undefined when the
+     *     transaction is completed, or rejects with an error if the transaction
+     *     is aborted or errors out
+     */
+    static async promiseForTransaction(transaction) {
+      return new Promise((resolve, reject) => {
+        transaction.oncomplete = () => { resolve(); };
+        transaction.onabort = event => { reject(event.target.error); };
+        transaction.onerror = event => { reject(event.target.error); };
+      });
+    }
+
+    /** Apply a database schema to an IndexedDB database.
+     *
+     * This must be called from a versionchange transaction.
+     *
+     * The current implementation builds the schema from scratch, i.e. does not
+     * support diffing.
+     *
+     * @param {Object} dbSchema the schema applied to the database
+     * @param {IDBDatabase} idbDatabase the database that the schema is applied
+     *     to; must be in the middle of a versionchange transaction
+     */
+    static applySchema(dbSchema, idbDatabase) {
+      for (let storeName in dbSchema) {
+        if (!dbSchema.hasOwnProperty(storeName)) continue;
+
+        const storeSchema = dbSchema[storeName];
+        DatabaseUtils.applySchemaStore(storeSchema, storeName, idbDatabase);
+      }
+    }
+
+    /** Apply a store schema to an IndexedDB object store.
+     *
+     * This must be called from a versionchange transaction.
+     *
+     * The current implementation builds the store from scratch, i.e. does not
+     * support diffing.
+     *
+     * @param {Object} storeSchema the schema applied to the object store
+     * @param {string} storeName the name given to the newly created store
+     * @param {IDBDatabase} idbDatabase the database that the schema is applied
+     *     to; must be in the middle of a versionchange transaction
+     * @return {IDBObjectStore} the newly created object store
+     */
+    static applySchemaStore(storeSchema, storeName, idbDatabase) {
+      const indexes = storeSchema.indexes || [];
+      const options = Object.assign({}, storeSchema);
+      delete options.indexes;
+
+      const idbStore = idbDatabase.createObjectStore(storeName, options);
+      for (let index of indexes) {
+        const indexOptions = Object.assign({}, index);
+        const indexName = index.name;
+        delete indexOptions.name;
+        const indexKey = index.keyPath;
+        delete indexOptions.keyPath;
+
+        idbStore.createIndex(indexName, indexKey, indexOptions);
+      }
+      return idbStore;
+    }
+  }
+
   class Database {
     /** Delete a database.
      *
      * @param {string} dbName The name of the databse to be deleted.
      */
     static async delete(dbName) {
-      return new Promise((resolve, reject) => {
-        const request = indexedDB.deleteDatabase(dbName);
-        request.onsuccess = () => { resolve(true); }
-        request.onerror = (event) => { reject(event.target.error); };
-      });
+      const request = indexedDB.deleteDatabase(dbName);
+      return await DatabaseUtils.promiseForRequest(request);
     }
 
     /** Open a database.
      *
-     * @param {string} dbName The name of the databse to be opened.
-     * @param {object} dbSchema Description of the database structure.
+     * @param {string} dbName the name of the database to be opened
+     * @param {object} dbSchema the description of the database structure
+     * @return {Promise<Database>} promise that resolves with a new Database
+     *     instance for the open database
      */
     static async open(dbName, dbSchema) {
-      return new Promise((resolve, reject) => {
-        const request = indexedDB.open(dbName, 1);
-        request.onerror = (event) => { reject(event.target.error); };
-        request.onsuccess = (event) => {
-          const idbDatabase = event.target.result;
-          const db = new Database(dbName, dbSchema, idbDatabase);
-          resolve(db);
-        };
-        request.onupgradeneeded = (event) => {
-          const idbDatabase = event.target.result;
-          for (let storeName in dbSchema) {
-            if (!dbSchema.hasOwnProperty(storeName)) continue;
+      const request = indexedDB.open(dbName, 1);
+      const result = await DatabaseUtils.promiseForOpenRequest(request);
 
-            const storeDetails = dbSchema[storeName];
-            const indexes = storeDetails.indexes || [];
-            const options = Object.assign({}, storeDetails);
-            delete options.indexes;
+      let idbDatabase = null;
+      if (result.transaction === null) {
+        idbDatabase = result.database;
+      } else {
+        DatabaseUtils.applySchema(dbSchema, result.database);
+        idbDatabase = await DatabaseUtils.promiseForRequest(request);
+      }
 
-            const store = idbDatabase.createObjectStore(storeName, options);
-            for (let index of indexes) {
-              const indexOptions = Object.assign({}, index);
-              const indexName = index.name;
-              delete indexOptions.name;
-              const indexKey = index.keyPath;
-              delete indexOptions.keyPath;
-
-              store.createIndex(indexName, indexKey, indexOptions);
-            }
-          }
-        };
-      });
+      return new Database(dbName, dbSchema, idbDatabase);
     }
 
-    /** Do not instantiate directly. Use Database.open() instead. */
+    /** Do not instantiate directly. Use Database.open() instead.
+     *
+     * @param {string} dbName the database's name
+     * @param {Object} dbSchema the database's schema
+     * @param {IDBDatabase} idbDatabase the IndexedDB instance wrapped by this
+     */
     constructor(dbName, dbSchema, idbDatabase) {
       this._dbName = dbName;
       this._dbSchema = dbSchema;
@@ -89,70 +194,104 @@
 
     /** Deletes all objects from a store.
      *
-     * @param [string] storeName The name of the store getting wiped
+     * @param {string} storeName the name of the store getting wiped
+     * @return {Promise<void>} promise that resolves with undefined when the
+     *     store is wiped
      */
     async clearStore(storeName) {
-      return new Promise((resolve, reject) => {
-        const transaction = this._idbDatabase.transaction([storeName],
-                                                          'readwrite');
-        transaction.onerror = (event) => { resolve(event.target.error); };
-        transaction.oncomplete = () => { resolve(); };
+      const transaction =
+          this._idbDatabase.transaction([storeName], 'readwrite');
+      const transactionPromise =
+          DatabaseUtils.promiseForTransaction(transaction);
 
-        const objectStore = transaction.objectStore(storeName);
-        const request = objectStore.clear();
-        request.onerror = (event) => { reject(event.target.error); };
-      });
+      const objectStore = transaction.objectStore(storeName);
+      const request = objectStore.clear();
+      await DatabaseUtils.promiseForRequest(request);
+      await transactionPromise;
     }
 
     /** Counts the number of objects that match a set of queries.
      *
-     * @param [array<{index?: string, range?: IDBKeyRange>] queries
-     * @param [IDBKeyRange?] keyRange The range of keys being counted; null
-     *                                if the entire object store / index is
-     *                                counted
+     * @param {Array<{index?: string, range?: IDBKeyRange>}>} queries array with
+     *     the queries; each query targets the index named |index|, or the
+     *     object store (if the index does not exist); each query targets the
+     *     |range| key range, or the entire object store / index if the range
+     *     does not exist
+     * @return {Promise<Array<number>} promise that resolves to an array with an
+     *     entry for each query; the entry is the number of records matching the
+     *     query
      */
     async count(storeName, queries) {
-      return new Promise((resolve, reject) => {
-        const results = [];
-        const transaction = this._idbDatabase.transaction([storeName],
-                                                          'readonly');
-        transaction.onerror = (event) => { resolve(event.target.error); };
-        transaction.oncomplete = () => { resolve(results); };
+      const transaction =
+          this._idbDatabase.transaction([storeName], 'readonly');
+      const transactionPromise =
+          DatabaseUtils.promiseForTransaction(transaction);
 
-        for (let query of queries) {
-          let resultIndex = results.length;
-          results.push(0);
-
-          const objectStore = transaction.objectStore(storeName);
-          const dataSource = ('index' in query) ? objectStore.index(query.index)
-                                                : objectStore;
-          const request = ('range' in query) ? dataSource.count(query.range)
-                                            : dataSource.count();
-
-          request.onerror = (event) => { reject(event.target.error); };
-          request.onsuccess = (event) => {
-            results[resultIndex] = event.target.result;
-          };
-        }
-      });
+      const objectStore = transaction.objectStore(storeName);
+      const resultPromises = [];
+      for (let query of queries) {
+        const dataSource = ('index' in query) ? objectStore.index(query.index)
+                                              : objectStore;
+        const request = ('range' in query) ? dataSource.count(query.range)
+                                           : dataSource.count();
+        resultPromises.push(DatabaseUtils.promiseForRequest(request));
+      }
+      const results = await Promise.all(resultPromises);
+      await transactionPromise;
+      return results;
     }
 
     /** Deletes an object from a store.
      *
-     * @param [string] storeName The name of the store losing an object
-     * @param [object] objectKey The primary key of the object to be deleted
+     * @param {string} storeName the name of the store losing an object
+     * @param {object} objectKey the primary key of the object to be deleted
+     * @return {Promise<void>} promise that resolves to undefined when the
+     *     object is deleted
      */
     async delete(storeName, objectKey) {
-      return new Promise((resolve, reject) => {
-        const transaction = this._idbDatabase.transaction([storeName],
-                                                          'readwrite');
-        transaction.onerror = (event) => { resolve(event.target.error); };
-        transaction.oncomplete = () => { resolve(); };
+      const transaction =
+          this._idbDatabase.transaction([storeName], 'readwrite');
+      const transactionPromise =
+          DatabaseUtils.promiseForTransaction(transaction);
 
-        const objectStore = transaction.objectStore(storeName);
-        const request = objectStore.delete(objectKey);
-        request.onerror = (event) => { reject(event.target.error); };
-      });
+      const objectStore = transaction.objectStore(storeName);
+      const request = objectStore.delete(objectKey);
+      await DatabaseUtils.promiseForRequest(request);
+      await transactionPromise;
+    }
+
+    /** Reads from a store by iterating a cursor.
+     *
+     * @param {string} storeName the name of the store being read
+     * @param {{index?: string, range?: IDBKeyRange}} query narrows down the
+     *     data being read
+     * @param {function(IDBCursor): boolean} cursorCallback called for each
+     *     cursor yielded by the iteration; must return a truthy value to
+     *     continue iteration, or a falsey value to stop iterating
+     */
+    async iterateCursor(storeName, query, cursorCallback) {
+      const transaction =
+          this._idbDatabase.transaction([storeName], 'readonly');
+      const transactionPromise =
+          DatabaseUtils.promiseForTransaction(transaction);
+
+      const objectStore = transaction.objectStore(storeName);
+      const dataSource = ('index' in query) ? objectStore.index(query.index)
+                                            : objectStore;
+      const request = ('range' in query) ? dataSource.openCursor(query.range)
+                                          : dataSource.openCursor();
+      while (true) {
+        const cursor = await DatabaseUtils.promiseForRequest(request);
+        if (!cursor)
+          break;  // The iteration completed.
+
+        const willContinue = cursorCallback(cursor);
+        if (!willContinue)
+          break;
+        cursor.continue();
+      }
+
+      await transactionPromise;
     }
 
     /** Reads from a store by iterating a cursor.
@@ -195,47 +334,50 @@
 
     /** Writes an array of objects to a store in a single transaction.
      *
-     * @param [string] storeName The name of the store receiving the objects
-     * @param [array<object>] objects The objects to be written to the store
+     * @param {string} storeName the name of the store receiving the objects
+     * @param {Array<object>} objects the objects written to the store
+     * @return {Promise<void>} promise that resolves to undefined when all the
+     *     objects are written
      */
     async putBatch(storeName, objects) {
-      return new Promise((resolve, reject) => {
-        const transaction = this._idbDatabase.transaction([storeName],
-                                                          'readwrite');
-        transaction.onerror = (event) => { resolve(event.target.error); };
-        transaction.oncomplete = () => { resolve(); };
+      const transaction =
+          this._idbDatabase.transaction([storeName], 'readwrite');
+      const transactionPromise =
+          DatabaseUtils.promiseForTransaction(transaction);
 
-        const objectStore = transaction.objectStore(storeName);
-        for (let object of objects) {
-          const request = objectStore.put(object);
-          request.onerror = (event) => { reject(event.target.error); };
-        }
-      });
+      const objectStore = transaction.objectStore(storeName);
+      const requestPromises = [];
+      for (let object of objects) {
+        const request = objectStore.put(object);
+        requestPromises.push(DatabaseUtils.promiseForRequest(request));
+      }
+      await Promise.all(requestPromises);
+      await transactionPromise;
     }
 
+    /** Reads and writes a modified object in a single transaction.
+     * @param {string} storeName the name of the changed object store
+     * @param {Object} objectKey the primary key of the changed object
+     * @param {function(Object) => Object} modifyCallback called with the old
+     *     object, must return the modified object
+     * @return {Promise<void>} promise that resolves to undefined when the
+     *     modified object is written to the database
+     */
     async readModifyWrite(storeName, objectKey, modifyCallback) {
-      return new Promise((resolve, reject) => {
-        const transaction = this._idbDatabase.transaction([storeName],
-                                                          'readwrite');
-        transaction.onerror = (event) => { resolve(event.target.error); };
-        transaction.oncomplete = () => { resolve(); };
+      const transaction =
+          this._idbDatabase.transaction([storeName], 'readwrite');
+      const transactionPromise =
+          DatabaseUtils.promiseForTransaction(transaction);
 
-        const objectStore = transaction.objectStore(storeName);
-        const getRequest = objectStore.get(objectKey);
-        getRequest.onerror = (event) => { resolve(event.target.error); };
-        getRequest.onsuccess = (event) => {
-          const oldObject = event.target.result;
-          let newObject;
-          try {
-            newObject = modifyCallback(oldObject);
-          } catch (callbackError) {
-            reject(callbackError);
-          }
-          const putRequest = (objectStore.keyPath === null) ?
-              objectStore.put(newObject, key) : objectStore.put(newObject);
-          putRequest.onerror = (event) => { resolve(event.target.error); };
-        };
-      });
+      const objectStore = transaction.objectStore(storeName);
+      const getRequest = objectStore.get(objectKey);
+      const oldObject = await DatabaseUtils.promiseForRequest(getRequest);
+
+      const newObject = modifyCallback(oldObject);
+      const putRequest = (objectStore.keyPath === null) ?
+          objectStore.put(newObject, objectKey) : objectStore.put(newObject);
+      await DatabaseUtils.promiseForRequest(putRequest);
+      await transactionPromise;
     }
   };
 
